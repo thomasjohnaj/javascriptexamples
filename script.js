4914876ed99fdd9fe7ebfd51b290f090c0244c49
Z003TCPN <thomas.arricat@siemens.com>
2022-07-25 07:04:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+setInterval(setClock,1000);
+function setClock() {
+    const hourHand = document.querySelector('[data-hour-hand]');
+    const minuteHand = document.querySelector('[data-minute-hand]');
+    const secondHand = document.querySelector('[data-second-hand]');
+
+    const currentDate = new Date();
+    const secondRatio = currentDate.getSeconds()/60;
+    const minuteRatio =(secondRatio +currentDate.getMinutes())/60;
+    const hourRatio = (minuteRatio + currentDate.getHours())/12;
+    setRotation(secondHand,secondRatio);
+    setRotation(minuteHand,minuteRatio);
+    setRotation(hourHand,hourRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation',rotationRatio * 360);
+}
+
+setClock();
